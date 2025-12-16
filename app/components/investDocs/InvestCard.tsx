6a421d137b947468ae9410104413a9ac2423e6b5
ulia-lukash <ulianalukash@gmail.com>
2025-12-16 21:42:16 +0300

    wip

--- app/components/investDocs/InvestCard.tsx
+++ app/components/investDocs/InvestCard.tsx
@@ -1,4 +1,5 @@
 export function InvestCard(props: {label: String, caption: String}) {
+    // TODO: icons
     return (
         <div className="invest-card">
             <div className="icon-circle"></div>
